7d67f8a75c169917592b1547b0aa341b3195f47d
Mas-Dietrich <MasDietrich@icloud.com>
2022-04-22 12:06:12 -0600

    all congress included, began filter buttons

--- congress/congress.js
+++ congress/congress.js
@@ -1,11 +1,14 @@
 import { senators } from '../starWars/data/senators.js'
-//import { representatives } from '..starWars/data/representatives.js'
+import { representatives } from '../starWars/data/representatives.js'
 
-//const allCongressMembers = [...senators, ...representatives]
+const allCongressMembers = [...senators, ...representatives]
 
-const senatorsDiv = document.querySelector('.senatorsDiv')
+const header = document.querySelector('header')
+
+const congressDiv = document.querySelector('.congressDiv')
 const seniorityHeading = document.querySelector('.seniority')
 const loyaltyList = document.querySelector('.loyaltyList')
+
 
 function simplifiedSenators() {
     return senators.map(senator => {
@@ -19,28 +22,73 @@ function simplifiedSenators() {
             seniority: +senator.seniority,
             missedVotesPct: senator.missed_votes_pct,
             loyaltyPct: senator.votes_with_party_pct,
+            birthday: senator.date_of_birth,
         }
     })
 }
 
+function simplifiedRepresentatives() {
+    return representatives.map(representative => {
+        const middleName = representative.middle_name ? ` ${representative.middle_name} ` : `  `
+        return{
+            id: representative.id,
+            name: `${representative.first_name}${middleName}${representative.last_name}`,
+            party: representative.party,
+            gender: representative.gender,
+            imgURL: `https://www.govtrack.us/static/legislator-photos/${representative.govtrack_id}-200px.jpeg`,
+            seniority: +representative.seniority,
+            missedVotesPct: representative.missed_votes_pct,
+            loyaltyPct: representative.votes_with_party_pct,
+            birthday: representative.date_of_birth,
+        }
+    })
+}
+
+const simpleRepresentatives = simplifiedRepresentatives()
 const simpleSenators = simplifiedSenators()
 
-function populateSenatorDiv(senatorArray) {
+/*function populateSenatorDiv(senatorArray) {
     senatorArray.forEach(senator => {
         const senFigure = document.createElement('figure')
         const figImg = document.createElement('img')
         const figCaption = document.createElement('figcaption')
 
         figImg.src = senator.imgURL
-        figCaption.textContent = senator.name
+        figCaption.textContent = `${senator.name} born ${senator.birthday}`
 
         senFigure.appendChild(figImg)
         senFigure.appendChild(figCaption)
         senatorsDiv.appendChild(senFigure)
     })
+} */
+function populateCongressDiv(congressArray) {
+    congressArray.forEach(senator => {
+        const senFigure = document.createElement('figure')
+        const figImg = document.createElement('img')
+        const figCaption = document.createElement('figcaption')
+
+        figImg.src = senator.imgURL
+        figCaption.textContent = `${senator.name} born ${senator.birthday}`
+
+        senFigure.appendChild(figImg)
+        senFigure.appendChild(figCaption)
+        congressDiv.appendChild(senFigure)
+    })
+    congressArray.forEach(representative => {
+        const repFigure = document.createElement('figure')
+        const repImg = document.createElement('img')
+        const repCaption = document.createElement('figcaption')
+
+        repImg.src= representative.imgURL
+        repCaption.textContent = `${representative.name} born ${representative.birthday}`
+        repFigure.appendChild(repImg)
+        repFigure.appendChild(repCaption)
+        congressDiv.appendChild(repFigure)
+    })
 }
 
-populateSenatorDiv(simpleSenators);
+populateCongressDiv(simpleSenators);
+populateCongressDiv(simpleRepresentatives);
 
 const mostSeniorMember = simplifiedSenators().reduce((acc, senator) => {
     return acc.seniority > senator.seniority ? acc : senator
@@ -48,11 +96,8 @@ const mostSeniorMember = simplifiedSenators().reduce((acc, senator) => {
 
 const biggestMissedVotesPct = simplifiedSenators().reduce((acc, senator) => acc.missedVotesPct > senator.missedVotesPct ? acc : senator)
 
-// console.log(biggestMissedVotesPct.missedVotesPct)
-
 const biggestVacationerList = simplifiedSenators().filter(senator => senator.missedVotesPct === biggestMissedVotesPct.missedVotesPct).map(senator => senator.name).join(' and ')
 
-// console.log(biggestVacationerList)
 
 seniorityHeading.textContent = `The most senior member of the senate is ${mostSeniorMember.name} and the biggest vacationers are ${biggestVacationerList}`
 
@@ -65,11 +110,26 @@ simplifiedSenators().forEach(senator => {
     }
 })
 
+const allCongress = document.createElement('button')
+allCongress.textContent = 'All Congress'
+allCongress.addEventListener('click', function() {
+    populateCongressDiv(simpleSenators)
+})
+
+const maleSenators = allCongressMembers.filter(senators => senators.gender === 'M')
+const maleSenatorButton = document.createElement('button')
+maleSenatorButton.textContent = 'Male Senators'
+maleSenatorButton.addEventListener('click', () => populateCongressDiv(maleSenators))
+
+header.appendChild(allCongress)
+header.appendChild(maleSenatorButton)
+
+
+//Buttons to get senators or representatives, male, or female
+
 // TODO suggestions for final project
 //Sorting by parting affiliations
 //More data with each congress member
-//ways to select members of the house of representatives
 //Add filter for oldest/youngest member
-//Add birthdate to the figcaptions of representatives
 
 

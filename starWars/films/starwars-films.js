d46725acfc34e4f1c14f983ae89d23d19e11433a
Mas-Dietrich <MasDietrich@icloud.com>
2022-04-27 16:52:35 -0600

    Added release dates and styled font

--- starWars/films/starwars-films.js
+++ starWars/films/starwars-films.js
@@ -10,14 +10,17 @@ for (let i = 0; i < films.length; i++) {
 let figure = document.createElement('figure')
 let figImage = document.createElement('img')
 let figcaption = document.createElement('figcaption')
+let movieCaption = document.createElement('p')
 
 let filmNum = getLastNumber(films[i].url)
 
 figImage.src = `https://starwars-visualguide.com/assets/img/films/${filmNum}.jpg`
 
 figcaption.textContent = films[i].title
+movieCaption.textContent = `Released ${films[i].release_date}`
 
 figure.appendChild(figImage)
 figure.appendChild(figcaption)
 filmList.appendChild(figure)
+figcaption.appendChild(movieCaption)
 }
